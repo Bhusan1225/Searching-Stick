7f554222b7aa89e9dcdf3e95097f98ebef32474d
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-06-12 22:23:24 +0530

    working

--- Searching-Stick/source/Gameplay/GameplayView.cpp
+++ Searching-Stick/source/Gameplay/GameplayView.cpp
@@ -1,1 +1,24 @@
 #include "Gameplay/GameplayView.h"
+#include "Gameplay/GameplayController.h"
+#include "Global/ServiceLocator.h"
+#include "Global/Config.h"
+
+namespace Gameplay
+{
+	using namespace UI::UIElement;
+	using namespace Global;
+
+	GameplayView::GameplayView() {}
+
+	GameplayView::~GameplayView() {}
+
+	void GameplayView::initialize(GameplayController* gameplay_controller)
+	{
+
+	}
+
+
+	void GameplayView::update() {}
+
+	void GameplayView::render() {}
+}
